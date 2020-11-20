af75ba4476ad9410e6aac8d9aa20294daf4f8d79
Demesmaeker <morgane.demesmaeker@gmail.com>
2020-11-20 15:30:01 +0100

    adding communes

--- App/Cleaning/functions.py
+++ App/Cleaning/functions.py
@@ -11,3 +11,13 @@ def set_surface(df):
     if df['surface_of_the_land'] < 2:
         if df['garden_area'] > 2:
             df['surface_of_the_land'] = df['house_area', 'garden_area', 'terrace_area'].sum(axis=1)
+
+
+
+def get_region(province):
+    if province == 'Bruxelles (19 communes)':
+        return 'Région Bruxelles-capitale'
+    elif province in ['Hainaut', 'Liège', 'Namur', 'Brabant wallon', 'Luxembourg']:
+        return 'Région wallonne'
+    else : 
+        return 'Région flamande'
